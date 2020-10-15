92097c009096d5d8ac129e3dcca0e73c2d2a28b7
jordansantosj <67442300+jordansantosj@users.noreply.github.com>
2020-10-15 05:05:47 -0300

    adicionado componente de avaliações

--- src/app/restaurant-detail/reviews/reviews.component.ts
+++ src/app/restaurant-detail/reviews/reviews.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs/Observable';
+import {ActivatedRoute} from '@angular/router';
+import {RestaurantsService} from '../../restaurants/restaurants.service'
 
 @Component({
   selector: 'mt-reviews',
@@ -6,9 +9,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ReviewsComponent implements OnInit {
 
-  constructor() { }
+  reviews: Observable<any>
+  constructor(private restaurantsService: RestaurantsService,
+    private route: ActivatedRoute) { }
 
   ngOnInit() {
+    this.reviews = this.restaurantsService.reviewsOfRestaurant(this.route.parent.snapshot.params['id'])
   }
 
 }
--- src/app/restaurants/restaurants.service.ts
+++ src/app/restaurants/restaurants.service.ts
@@ -23,6 +23,11 @@ export class RestaurantsService {
         return this.http.get(`${MEAT_API}/restaurants/${id}`)
         .map(response => response.json())
         .catch(ErrorHandler.handleError)
+    }
 
+    reviewsOfRestaurant(id: string): Observable<any>{
+        return this.http.get(`${MEAT_API}/restaurants/${id}/reviews`)
+        .map(response => response.json())
+        .catch(ErrorHandler.handleError)
     }
 }
