957b0fe9e9424c64df36cd09d4d045f6398758ed
jordansantosj <67442300+jordansantosj@users.noreply.github.com>
2020-10-24 02:49:10 -0300

    Mudança de localização para pt-BR

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, LOCALE_ID } from '@angular/core';
 import { HttpModule } from '@angular/http';
 import { RouterModule } from '@angular/router';
 
@@ -38,7 +38,7 @@ import {ShoppingCartService} from './restaurant-detail/shopping-cart/shopping-ca
     HttpModule,
     RouterModule.forRoot(ROUTES)
   ],
-  providers: [RestaurantsService, ShoppingCartService],
+  providers: [RestaurantsService, ShoppingCartService, {provide: LOCALE_ID, useValue: 'pt-BR'}],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
