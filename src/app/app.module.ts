63e446f6b5e142e1159916b5b8c00e8b268274fe
jordansantosj <67442300+jordansantosj@users.noreply.github.com>
2020-10-24 02:42:41 -0300

    Adicionado component de carrinho de compras

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -17,6 +17,7 @@ import { MenuComponent } from './restaurant-detail/menu/menu.component';
 import { ShoppingCartComponent } from './restaurant-detail/shopping-cart/shopping-cart.component';
 import { MenuItemComponent } from './restaurant-detail/menu-item/menu-item.component';
 import { ReviewsComponent } from './restaurant-detail/reviews/reviews.component';
+import {ShoppingCartService} from './restaurant-detail/shopping-cart/shopping-cart.service'
 
 @NgModule({
   declarations: [
@@ -37,7 +38,7 @@ import { ReviewsComponent } from './restaurant-detail/reviews/reviews.component'
     HttpModule,
     RouterModule.forRoot(ROUTES)
   ],
-  providers: [RestaurantsService],
+  providers: [RestaurantsService, ShoppingCartService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
